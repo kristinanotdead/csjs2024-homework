d2fcbc66e5197e8f8c9ef56da798a0cc39fc7ee8
kristinanotdead <solkristinanotdead2002@gmail.com>
2024-03-09 20:18:22 +0300

    Сделаны задания 2 и 3

--- src/ru/croc/javaschool2024/soloveva/task2/Task2.java
+++ src/ru/croc/javaschool2024/soloveva/task2/Task2.java
@@ -1,9 +1,35 @@
 package ru.croc.javaschool2024.soloveva.task2;
 
-
 public class Task2 {
-
-
+    public static String getSuffix(int countDiv) {
+        switch (countDiv) {
+            case 0:
+                return "B";
+            case 1:
+                return "KB";
+            case 2:
+                return "MB";
+            case 3:
+                return "GB";
+            case 4:
+                return "TB";
+        }
+        return "Too big";
+    }
+    public static void printBytes(double in) {
+        int countDiv = 0;
+        while (in >= 1024) {
+            in /= 1024;
+            countDiv++;
+        }
+        String suffix = getSuffix(countDiv);
+        System.out.printf("%.1f %s", in, suffix);
+    }
     public static void main(String[] args) {
+        try{
+            printBytes(Long.parseLong(args[0]));
+        } catch (NumberFormatException ex){
+            System.out.println("Неправильный формат ввода");
+        }
     }
 }
--- src/ru/croc/javaschool2024/soloveva/task3/Task3.java
+++ src/ru/croc/javaschool2024/soloveva/task3/Task3.java
@@ -1,6 +1,27 @@
 package ru.croc.javaschool2024.soloveva.task3;
 
 public class Task3 {
+    public static void findSumOfArithmeticProgr(int start, int plus, int amount) {
+        int sum = start;
+        int el = start;
+
+        for (int i = 0; i < amount - 1; i++) {
+            el += plus;
+            sum += el;
+        }
+
+        System.out.println(sum);
+    }
+
     public static void main(String[] args) {
+        try {
+            findSumOfArithmeticProgr(
+                    Integer.parseInt(args[0]),
+                    Integer.parseInt(args[1]),
+                    Integer.parseInt(args[2])
+            );
+        } catch (NumberFormatException ex) {
+            System.out.println("Неправильный формат ввода");
+        }
     }
 }
