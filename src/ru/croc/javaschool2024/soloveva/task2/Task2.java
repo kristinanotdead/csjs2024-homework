73363ddb612dbd955e6f2abb401d34fe52f9d3f1
kristinanotdead <solkristinanotdead2002@gmail.com>
2024-03-15 02:29:05 +0300

    Поправлены 2 и 3 задачи

--- src/ru/croc/javaschool2024/soloveva/task2/Task2.java
+++ src/ru/croc/javaschool2024/soloveva/task2/Task2.java
@@ -16,20 +16,23 @@ public class Task2 {
         }
         return "Too big";
     }
-    public static void printBytes(double in) {
+    public static void printBytes(long in) {
         int countDiv = 0;
-        while (in >= 1024) {
-            in /= 1024;
+        double val = in;
+        while (val >= 1024) {
+            val /= 1024;
             countDiv++;
         }
         String suffix = getSuffix(countDiv);
-        System.out.printf("%.1f %s", in, suffix);
+        System.out.printf("%.1f %s", val, suffix);
     }
     public static void main(String[] args) {
         try{
             printBytes(Long.parseLong(args[0]));
         } catch (NumberFormatException ex){
             System.out.println("Неправильный формат ввода");
+        } catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Нет входных данных");
         }
     }
 }
--- src/ru/croc/javaschool2024/soloveva/task3/Task3.java
+++ src/ru/croc/javaschool2024/soloveva/task3/Task3.java
@@ -1,27 +1,31 @@
 package ru.croc.javaschool2024.soloveva.task3;
 
 public class Task3 {
-    public static void findSumOfArithmeticProgr(int start, int plus, int amount) {
-        int sum = start;
-        int el = start;
+    public static long findSumOfArithmeticProgr(int start, int plus, int amount) {
+        long sum = start;
+        long el = start;
 
         for (int i = 0; i < amount - 1; i++) {
             el += plus;
             sum += el;
         }
 
-        System.out.println(sum);
+        return sum;
     }
 
     public static void main(String[] args) {
         try {
-            findSumOfArithmeticProgr(
+            long sum = findSumOfArithmeticProgr(
                     Integer.parseInt(args[0]),
                     Integer.parseInt(args[1]),
                     Integer.parseInt(args[2])
             );
+
+            System.out.println(sum);
         } catch (NumberFormatException ex) {
             System.out.println("Неправильный формат ввода");
+        } catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Нет входных данных");
         }
     }
 }
